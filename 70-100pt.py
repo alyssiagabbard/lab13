b04070f57b94ffdca58458669a3c9d241b6d062d
alyssiagabbard <s262078@student.roundrockisd.org>
2014-10-31 14:43:35 -0500

    70 point

--- 70-100pt.py
+++ 70-100pt.py
@@ -27,10 +27,25 @@ class MyApp:
        	    self.myContainer1 = Frame(parent)
        	    self.myContainer1.pack()
        	    self.up = Button(self.myContainer1)
-       	    self.up.configure(text="up", background= "green")
+       	    self.up.configure(text="Up", background= "green")
        	    self.up.grid(row=0,column=0)
        	    # Bind an event to the first button
        	    self.up.bind("<Button-1>", self.upClicked)
+       	    
+       	    self.button1 = Button(self.myContainer1)
+       	    self.button1.configure(text="Left", background="blue")
+       	    self.button1.grid(row=0, column=1)
+       	    self.button1.bind("<Button-1>", self.button1Clicked)
+       	    
+       	    self.button2 = Button(self.myContainer1)
+       	    self.button2.configure(text="Right", background="orange")
+       	    self.button2.grid(row=0, column=2)
+       	    self.button2.bind("<Button-1>", self.button2Clicked)
+       	    
+       	    self.button3 = Button(self.myContainer1)
+       	    self.button3.configure(text="Down", background="red")
+       	    self.button3.grid(row=1, column=3)
+       	    self.button3.bind("<Button-1>", self.button3Clicked)
        	    
        	    # No need to edit this - just includes the drawpad into our frame
        	    drawpad.pack(side=BOTTOM)
@@ -39,7 +54,21 @@ class MyApp:
 	    global drawpad
 	    global player
 	    # Remember to include your "enemies" with "global"
-	    	
+	    
+	def button1Clicked(self, event):
+            global drawpad
+            global player
+            drawpad.move(player,-20,0)
+            
+        def button2Clicked(self, event):
+            global player
+            global drawpad
+            drawpad.move(player,20,0)
+            
+        def button3Clicked(self,event):
+            global player
+            global drawpad
+            drawpad.move(player,0,-20)	
 		
 	def upClicked(self, event):   
 	   global oval
